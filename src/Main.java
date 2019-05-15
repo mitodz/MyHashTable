5081582c722e39b0cd63dbe3ad958697cc66628b
mitodz <mitodzzz@gmail.com>
2019-05-15 13:34:07 +0300

    On the way to solve the problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner("5\n" +
+                "12\n" +
+                "add world\n" +
+                "add HellO\n" +
+                "check 4\n" +
+                "find World\n" +
+                "find world\n" +
+                "del world\n" +
+                "check 4\n" +
+                "del HellO\n" +
+                "add luck\n" +
+                "add GooD\n" +
+                "check 2\n" +
+                "del good");
         int m = sc.nextInt(); //размер таблицы
         int n = sc.nextInt(); //количество запросов
         MyHash book = new MyHash(m);
