0cbcfac26ac932717b7b00e64c6890e55f2030ff
mitodz <mitodzzz@gmail.com>
2019-05-15 13:29:44 +0300

    On the way to solve the problem

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int d = sc.nextInt();
-        MyHash book = new MyHash();
-        for (int i = 0; i < d; i++) {
+        int m = sc.nextInt(); //размер таблицы
+        int n = sc.nextInt(); //количество запросов
+        MyHash book = new MyHash(m);
+        for (int i = 0; i < n; i++) {
             String buf = sc.next();
             switch (buf) {
                 case "add":
-                    book.add(sc.nextInt(), sc.next());
+                    book.add(sc.next());
                     break;
                 case "del":
-                    book.del(sc.nextInt());
+                    book.del(sc.next());
                     break;
                 case "find":
-                    System.out.println(book.find(sc.nextInt()));
+                    System.out.println(book.find(sc.next()));
                     break;
+                case "check":
+                    StringNode temp = book.check(sc.nextInt());
+                    book.printStringNode(temp);
             }
         }
     }
